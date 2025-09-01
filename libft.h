7305d5f6874d7207d4c1f92f6fd99f5e267e3407
spacotto <silvia.pacotto@gmail.com>
2025-09-01 22:43:54 +0200

    Update libft.h

--- libft.h
+++ libft.h
@@ -38,3 +38,23 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n);
 /*MALLOC*/
 void*	ft_calloc(size_t n, size_t size);
 char*	ft_strdup(const char *s);
+
+/*Additional functions*/
+char *ft_substr(char const *s, unsigned int start, size_t len);
+char *ft_strjoin(char const *s1, char const *s2);
+char *ft_strtrim(char const *s1, char const *set);
+char **ft_split(char const *s, char c);
+char *ft_itoa(int n);
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void ft_striteri(char *s, void (*f)(unsigned int, char*));
+void ft_putchar_fd(char c, int fd);
+void ft_putstr_fd(char *s, int fd);
+void ft_putendl_fd(char *s, int fd);
+void ft_putnbr_fd(int n, int fd);
+
+/*Bonus part*/
+typedef struct s_list
+{
+void *content;
+struct s_list *next;
+} t_list;
