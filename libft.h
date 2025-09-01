1e9bb6714311516d6234eb67598714a63fda500f
spacotto <spacotto@student.42lehavre.fr>
2025-09-01 22:29:29 +0200

    header update

--- libft.h
+++ libft.h
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-int  ft_isalpha(int c);
-int  ft_isdigit(int c);
-int  ft_isalnum(int c);
-int  ft_isascii(int c);
-int  ft_isprint(int c);
-size_t  ft_strlen(const char *s);
-void*  ft_memset(void *s, int c, size_t n);
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void*	ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void*	ft_memcpy(void *dest, const void *src, size_t n);
 void*	ft_memmove(void *dest, const void *src, size_t n);
@@ -22,6 +22,6 @@ int	ft_strncmp(size_t n, const char s1, const char s2, size_t n);
 void*	ft_memchr(const void *s, int c, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 char*	ft_strnstr(const char *big, const char *little, size_t len);
-int  ft_atoi(const char *nptr);
-void *calloc(size_t n, size_t size);
-char*	strdup(const char *s);
+int	ft_atoi(const char *nptr);
+void*	ft_calloc(size_t n, size_t size);
+char*	ft_strdup(const char *s);
