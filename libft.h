8aeca55344c62e85642c6bf97b4ca6239cb1abc9
spacotto <spacotto@student.42lehavre.fr>
2025-09-01 17:22:52 +0200

    header change

--- libft.h
+++ libft.h
@@ -2,26 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
 
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char*	ft_strchr(const char *s, int c);
+char*	ft_strrchr(const char *s, int c);
+int	ft_strncmp(size_t n, const char s1, const char s2, size_t n);
+char*	ft_strnstr(const char *big, const char *little, size_t len);
+char*	strdup(const char *s);
 
 void*	ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void*	ft_memcpy(void *dest, const void *src, size_t n);
-void*	ft_memccpy(void *dest, const void *src, int c, size_t n);
 void*	ft_memmove(void *dest, const void *src, size_t n);
 void*	ft_memchr(const void *s, int c, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
-
-size_t	ft_strlen(const char *s);
-char*	ft_strdup(const char *s);
-char*	ft_strcpy(char *restrict dst, const char *restrict src);
-char*	ft_strncpy(char *dst, const char *restrict src, size_t dsize);
-char*	ft_strcat(char *restrict dst, const char *restrict src);
-char*	ft_strncat(char *restrict dst, const char *src, size_t ssize);
-char*	ft_strlcat(char *dst, const char *src, size_t size);
-char*	ft_strchr(const char *s, int c);
-char*	ft_strrchr(const char *s, int c);
-char*	ft_strstr(const char *haystack, const char *needle);
-char*	ft_strnstr(const char *big, const char *little, size_t len);
-int	ft_strcmp(const char *s1, const char *s2);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
