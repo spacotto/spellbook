033482d715a62cce49e8e652cf2427aabc949040
Silvia Pacotto <spacotto@c1r7p6.42lehavre.fr>
2025-10-13 15:30:10 +0200

    updated isalpha

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,3 +1,5 @@
+#include "libft.h"
+
 int	ft_isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
@@ -20,15 +22,4 @@ int	ft_isalpha(int c)
 		return (1);
 	else
 		return (0);
-}
-
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-int	main(int ac, char **av)
-{
-	(void)ac;
-	printf("ORIGINAL | %d\nCUSTOM   | %d\n", isalpha(atoi(av[1])), ft_isalpha(atoi(av[1])));
-	return(0);
 }
