ffedb7110d5aa2de55b7f96be9d24d4315d510d1
spacotto <silvia.pacotto@gmail.com>
2025-10-13 14:00:33 +0200

    Update libft.h

--- libft.h
+++ libft.h
@@ -50,7 +50,7 @@ void*    ft_memchr(const void *s, int c, size_t n);
 int      ft_memcmp(const void *s1, const void *s2, size_t n);
 char*	   ft_strnstr(const char *big, const char *little, size_t len);
 int      ft_atoi(const char *nptr);
-void*	   ft_calloc(size_t n, size_t size);
+void*	   ft_calloc(size_t nmemb, size_t size);
 char*	   ft_strdup(const char *s);
 
 /* MANDATORY PART: 42 FUNCTIONS */
