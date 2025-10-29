305320f963b8fc693889faca988c88bb3396e1fa
Silvia Pacotto <spacotto@c1r7p7.42lehavre.fr>
2025-10-29 16:19:05 +0100

    tests update

--- tests/test_clib.sh
+++ tests/test_clib.sh
@@ -17,3 +17,5 @@ cc -Wall -Wextra -Werror ft_bzero_test.c libft.a -o libft -lbsd
 ./libft
 cc -Wall -Wextra -Werror ft_memcpy_test.c libft.a -o libft -lbsd 
 ./libft
+cc -Wall -Wextra -Werror ft_memmove_test.c libft.a -o libft -lbsd 
+./libft
