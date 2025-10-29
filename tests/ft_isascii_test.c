e8f56a0bfd3239f78058ee75aeb68b6cb1ac02fb
Silvia Pacotto <spacotto@c1r7p7.42lehavre.fr>
2025-10-29 14:21:05 +0100

    tests update

--- tests/ft_isascii_test.c
+++ tests/ft_isascii_test.c
@@ -1,30 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isdigit_test.c                                  :+:      :+:    :+:   */
+/*   ft_isascii_test.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: spacotto <spacotto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/29 11:41:39 by spacotto          #+#    #+#             */
-/*   Updated: 2025/10/29 11:44:03 by spacotto         ###   ########.fr       */
+/*   Created: 2025/10/29 14:16:21 by spacotto          #+#    #+#             */
+/*   Updated: 2025/10/29 14:18:24 by spacotto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	main()
+int main()
 {
-	puts("\n================================= ft_isdigit.c\n");
+	puts("\n================================= ft_isascii.c\n");
+
 	{
-		int tests[] = {31, 32, 47, 48, 57, 58, 126, 127, 300, 0, -1};
+		int tests[] = {1, 2, 3, 125, 126, 127, 300, 0, -1};
 		for (int i = 0; tests[i]; i++)
 		{
-			int og = isdigit(tests[i]);
-			int ft = ft_isdigit(tests[i]);
+			int og = isascii(tests[i]);
+			int ft = ft_isascii(tests[i]);
 			printf("TEST: %d ('%c')\nORIGINAL[%d] CUSTOM[%d]\n=== FINAL CHECK: %s\n\n",
-			tests[i], tests[i], og, ft,
-			(!!og == !!ft) ? "OK!" : "KO!");
+					tests[i], tests[i], og, ft,
+					(!!og == !!ft) ? "OK!" : "KO!");
 		}
 	}
+
 	return (0);
 }
---
+++ tests/test_clib.sh
@@ -0,0 +1,9 @@
+clear
+cc -Wall -Wextra -Werror ft_isalpha_test.c libft.a -o libft -lbsd
+./libft
+cc -Wall -Wextra -Werror ft_isdigit_test.c libft.a -o libft -lbsd 
+./libft
+cc -Wall -Wextra -Werror ft_isalnum_test.c libft.a -o libft -lbsd 
+./libft
+cc -Wall -Wextra -Werror ft_isascii_test.c libft.a -o libft -lbsd 
+./libft
