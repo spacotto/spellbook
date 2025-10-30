614cb32e842e011af47ce82f4daddf392f087093
Silvia Pacotto <spacotto@c1r6p9.42lehavre.fr>
2025-10-30 11:25:15 +0100

    lstsize

--- tests/test_bonus.sh
+++ tests/test_bonus.sh
@@ -3,3 +3,5 @@ cc -Wall -Wextra -Werror ft_lstnew_test.c libft.a -o libft -lbsd
 ./libft
 cc -Wall -Wextra -Werror ft_lstadd_front_test.c libft.a -o libft -lbsd
 ./libft
+cc -Wall -Wextra -Werror ft_lstsize_test.c libft.a -o libft -lbsd
+./libft
