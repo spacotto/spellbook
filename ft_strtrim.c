a14037186c663d33a85c146c737535561e22d09d
Silvia Pacotto <spacotto@c1r6p9.42lehavre.fr>
2025-10-30 16:54:22 +0100

    trim fix

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,37 +6,44 @@
 /*   By: spacotto <spacotto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 16:30:03 by spacotto          #+#    #+#             */
-/*   Updated: 2025/10/27 12:17:14 by spacotto         ###   ########.fr       */
+/*   Updated: 2025/10/30 16:28:43 by spacotto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_trimchr(char const *set, char c)
+{
+	while (*set)
+	{
+		if (*set == c)
+			return (1);
+		set++;
+	}
+	return (0);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*trim;
-	char	*start;
-	char	*end;
 	t_trim	t;
 
 	if (!s1)
 		return (NULL);
-	start = (char *)s1;
-	end = (char *)s1 + ft_strlen(s1) - 1;
-	t.setlen = ft_strlen(set);
-	t.i = ft_strlen(s1) - t.setlen;
-	trim = ft_calloc(ft_strlen(s1), sizeof(char));
-	if (!trim)
+	if (!*s1)
+		return (ft_calloc(1, sizeof(char)));
+	t.start = (char *)s1;
+	while (*t.start && ft_trimchr(set, *t.start))
+		t.start++;
+	if (!*t.start)
+		return (ft_calloc(1, sizeof(char)));
+	t.end = (char *)t.start + ft_strlen(t.start) - 1;
+	while (t.start < t.end && ft_trimchr(set, *t.end))
+		t.end--;
+	t.trimlen = (t.end - t.start) + 1;
+	t.trim = ft_calloc(t.trimlen + 1, sizeof(char));
+	if (!t.trim)
 		return (NULL);
-	else
-	{
-		if (*start && ft_strncmp(s1, set, t.setlen) == 0)
-			start += t.setlen;
-		if (end > start && *end && ft_strncmp(&s1[t.i], set, t.setlen) == 0)
-			end -= t.setlen;
-		t.trimlen = (end - start) + 1;
-		ft_memcpy(trim, start, t.trimlen);
-		trim[t.trimlen] = '\0';
-		return (trim);
-	}
+	ft_memcpy(t.trim, t.start, t.trimlen);
+	t.trim[t.trimlen] = '\0';
+	return (t.trim);
 }
