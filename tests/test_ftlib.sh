077a77339b5b934732be3e220f489b0f836b6b35
Silvia Pacotto <spacotto@c1r6p9.42lehavre.fr>
2025-10-30 11:57:00 +0100

    tests update strjoin

--- tests/test_ftlib.sh
+++ tests/test_ftlib.sh
@@ -1,3 +1,23 @@
 clear
 cc -Wall -Wextra -Werror ft_substr_test.c libft.a -o libft -lbsd
 ./libft
+cc -Wall -Wextra -Werror ft_strjoin_test.c libft.a -o libft -lbsd
+./libft
+#cc -Wall -Wextra -Werror ft_strtrim_test.c libft.a -o libft -lbsd
+#./libft
+#cc -Wall -Wextra -Werror ft_split_test.c libft.a -o libft -lbsd
+#./libft
+#cc -Wall -Wextra -Werror ft_itoa_test.c libft.a -o libft -lbsd
+#./libft
+#cc -Wall -Wextra -Werror ft_strmapi_test.c libft.a -o libft -lbsd
+#./libft
+#cc -Wall -Wextra -Werror ft_striteri_test.c libft.a -o libft -lbsd
+#./libft
+#cc -Wall -Wextra -Werror ft_putchar_fd_test.c libft.a -o libft -lbsd
+#./libft
+#cc -Wall -Wextra -Werror ft_putstr_fd_test.c libft.a -o libft -lbsd
+#./libft
+#cc -Wall -Wextra -Werror ft_putendl_fd_test.c libft.a -o libft -lbsd
+#./libft
+#cc -Wall -Wextra -Werror ft_putnbr_fd_test.c libft.a -o libft -lbsd
+#./libft
