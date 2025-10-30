97a647765b2592dfd5c1a49b0ce75688207bd502
Silvia Pacotto <spacotto@c1r6p9.42lehavre.fr>
2025-10-30 16:54:51 +0100

    the wrong trim

---
+++ ft_strtrim_block.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spacotto <spacotto@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/20 16:30:03 by spacotto          #+#    #+#             */
+/*   Updated: 2025/10/27 12:17:14 by spacotto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*trim;
+	char	*start;
+	char	*end;
+	t_trim	t;
+
+	if (!s1)
+		return (NULL);
+	start = (char *)s1;
+	end = (char *)s1 + ft_strlen(s1) - 1;
+	t.setlen = ft_strlen(set);
+	t.i = ft_strlen(s1) - t.setlen;
+	trim = ft_calloc(ft_strlen(s1), sizeof(char));
+	if (!trim)
+		return (NULL);
+	else
+	{
+		if (*start && ft_strncmp(s1, set, t.setlen) == 0)
+			start += t.setlen;
+		if (end > start && *end && ft_strncmp(&s1[t.i], set, t.setlen) == 0)
+			end -= t.setlen;
+		t.trimlen = (end - start) + 1;
+		ft_memcpy(trim, start, t.trimlen);
+		trim[t.trimlen] = '\0';
+		return (trim);
+	}
+}
