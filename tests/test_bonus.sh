0bfec184b2aff94065660c09ff93d4dde502d27b
Silvia Pacotto <spacotto@c1r6p9.42lehavre.fr>
2025-10-30 11:31:32 +0100

    tests update lstadd_back

--- tests/test_bonus.sh
+++ tests/test_bonus.sh
@@ -7,3 +7,5 @@ cc -Wall -Wextra -Werror ft_lstsize_test.c libft.a -o libft -lbsd
 ./libft
 cc -Wall -Wextra -Werror ft_lstlast_test.c libft.a -o libft -lbsd
 ./libft
+cc -Wall -Wextra -Werror ft_lstadd_back_test.c libft.a -o libft -lbsd
+./libft
