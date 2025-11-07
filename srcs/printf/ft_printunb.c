18555da38fc73e34f1d37dbf104472a08f6d9f1e
Silvia Pacotto <spacotto@c1r7p6.42lehavre.fr>
2025-11-07 15:13:49 +0100

    added printunb

---
+++ srcs/printf/ft_printunb.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printunb.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spacotto <spacotto@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/07 14:54:24 by spacotto          #+#    #+#             */
+/*   Updated: 2025/11/07 15:00:21 by spacotto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    ft_printunb(t_format *sformat)
+{
+	size_t	u;
+	char	*s;
+	size_t	len;
+
+	u = (size_t)va_arg(sformat->args, unsigned int);
+	s = ft_utoa_base(u, "0123456789");
+	ft_putstr_fd(s, 1);
+	len = ft_strlen(s);
+	sformat->print_len += len;
+}
