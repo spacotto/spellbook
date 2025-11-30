03709e45b2ca52ec3ee28f959b54033816158dd8
spacotto <spacotto@student.42lehavre.fr>
2025-12-01 00:06:49 +0100

    srcs update

---
+++ inc/get_next_line.h
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spacotto <spacotto@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 11:22:14 by spacotto          #+#    #+#             */
+/*   Updated: 2025/12/01 00:02:55 by spacotto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+// ============================================================================
+// INCLUDES
+// ============================================================================
+
+#include "libft.h"
+
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+// ============================================================================
+// DEFINES
+// ============================================================================
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42 
+# endif
+
+# ifndef FD_MAX
+#  define FD_MAX 1024
+# endif
+
+// ============================================================================
+// STRUCTURES
+// ============================================================================
+
+typedef struct s_buffer
+{
+	char	buffer[BUFFER_SIZE];
+	char	*new_line;
+	char	*start;
+	char	*end;
+}	t_buffer;
+
+typedef struct s_line
+{
+	char	*line;
+	ssize_t	bytes_read;
+}	t_line;
+
+// ============================================================================
+// PROTOTYPES
+// ============================================================================
+
+char	*get_next_line(int fd);
+
+// ============================================================================
+// COLORS
+// ============================================================================
+
+# define RESET 			"\033[0m"
+
+# define GRAY 			"\033[0;90m"
+# define RED 			"\033[0;91m"
+# define GREEN 			"\033[0;92m"
+# define YELLOW 		"\033[0;93m"
+# define BLUE 			"\033[0;94m"
+# define MAGENTA		"\033[0;95m"
+# define CYAN 			"\033[0;96m"
+# define WHITE 			"\033[0;97m"
+
+# define BOLD_GRAY 		"\033[1;90m"
+# define BOLD_RED 		"\033[1;91m"
+# define BOLD_GREEN 	"\033[1;92m"
+# define BOLD_YELLOW	"\033[1;93m"
+# define BOLD_BLUE		"\033[1;94m"
+# define BOLD_MAGENTA 	"\033[1;95m"
+# define BOLD_CYAN 		"\033[1;96m"
+# define BOLD_WHITE 	"\033[1;97m"
+
+#endif
