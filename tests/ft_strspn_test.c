14e8e213558f5e7b4197b7cd98f36f0678a0d086
Silvia Pacotto <spacotto@c1r7p7.42lehavre.fr>
2026-01-07 16:17:04 +0100

    update strspn

--- tests/ft_strspn_test.c
+++ tests/ft_strspn_test.c
@@ -6,7 +6,7 @@
 /*   By: spacotto <spacotto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 15:08:29 by spacotto          #+#    #+#             */
-/*   Updated: 2026/01/07 15:28:24 by spacotto         ###   ########.fr       */
+/*   Updated: 2026/01/07 16:16:58 by spacotto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,9 +46,100 @@ int	main()
 
 	{
 		const char s[1024] = "Homo quisque faber ipse fortunae sua";
-		const char *accept = "omoH";
+		const char *accept = "moH";
 	
 		ft_printf("=== Standard Case n.2\n");
+		ft_printf("String   | %s\n", s);
+		ft_printf("Accept   | %s\n", accept);
+		ft_printf("Result   | %d\n", ft_strspn(s, accept));
+		ft_printf("Expected | %d\n", strspn(s, accept));
+		
+		check (s, accept);
+	}
+
+	{
+		const char s[1024] = "Homo quisque faber ipse fortunae sua";
+		const char *accept = "fab";
+	
+		ft_printf("=== Standard Case n.3\n");
+		ft_printf("String   | %s\n", s);
+		ft_printf("Accept   | %s\n", accept);
+		ft_printf("Result   | %d\n", ft_strspn(s, accept));
+		ft_printf("Expected | %d\n", strspn(s, accept));
+		
+		check (s, accept);
+	}
+
+	{
+		const char s[1024] = "QWERT";
+		const char *accept = "QWERT";
+	
+		ft_printf("=== Full Match n.1\n");
+		ft_printf("String   | %s\n", s);
+		ft_printf("Accept   | %s\n", accept);
+		ft_printf("Result   | %d\n", ft_strspn(s, accept));
+		ft_printf("Expected | %d\n", strspn(s, accept));
+		
+		check (s, accept);
+	}
+
+	{
+		const char s[1024] = "QWERT";
+		const char *accept = "TREWQ";
+	
+		ft_printf("=== Full Match n.2\n");
+		ft_printf("String   | %s\n", s);
+		ft_printf("Accept   | %s\n", accept);
+		ft_printf("Result   | %d\n", ft_strspn(s, accept));
+		ft_printf("Expected | %d\n", strspn(s, accept));
+		
+		check (s, accept);
+	}
+
+	{
+		const char s[1024] = "QQWWEERRTT";
+		const char *accept = "QWERT";
+	
+		ft_printf("=== Full Match n.3\n");
+		ft_printf("String   | %s\n", s);
+		ft_printf("Accept   | %s\n", accept);
+		ft_printf("Result   | %d\n", ft_strspn(s, accept));
+		ft_printf("Expected | %d\n", strspn(s, accept));
+		
+		check (s, accept);
+	}
+
+	{
+		const char s[1024] = "";
+		const char *accept = "";
+	
+		ft_printf("=== Empty s AND accept\n");
+		ft_printf("String   | %s\n", s);
+		ft_printf("Accept   | %s\n", accept);
+		ft_printf("Result   | %d\n", ft_strspn(s, accept));
+		ft_printf("Expected | %d\n", strspn(s, accept));
+		
+		check (s, accept);
+	}
+
+	{
+		const char s[1024] = "";
+		const char *accept = "Nam";
+	
+		ft_printf("=== Empty s\n");
+		ft_printf("String   | %s\n", s);
+		ft_printf("Accept   | %s\n", accept);
+		ft_printf("Result   | %d\n", ft_strspn(s, accept));
+		ft_printf("Expected | %d\n", strspn(s, accept));
+		
+		check (s, accept);
+	}
+
+	{
+		const char s[1024] = "Nulli tacuisse nocet, nocet esse locutum.";
+		const char *accept = "";
+	
+		ft_printf("=== Empty accept\n");
 		ft_printf("String   | %s\n", s);
 		ft_printf("Accept   | %s\n", accept);
 		ft_printf("Result   | %d\n", ft_strspn(s, accept));
